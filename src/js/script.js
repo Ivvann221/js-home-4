447189d92625fd313d7650af8fc35652c689373e
Ivvann221 <94067148+Ivvann221@users.noreply.github.com>
2023-02-13 10:53:01 +0200

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,17 @@
+// 1
+
+// let numberOne = prompt('Введіть число');
+// let numberTwo = prompt('Введіть степінь');
+
+// function pow(x,n){
+// return Math.pow(x,n)
+// }
+
+// console.log(pow(numberOne,numberTwo));
+
+// 2
+
+
 let numberOne = prompt('Введіть число');
-let numberTwo = prompt('Введіть степінь');
+let numberTwo = prompt('Введіть число');
 
-function pow(x,n){
-return Math.pow(x,n)
-}
-
-console.log(pow(numberOne,numberTwo));
